c10026620c1f68ff85fa6fbe0e2836567828a89f
Parthi200 <thennavaiparthiban@gmail.com>
2024-08-25 13:18:45 +0530

    Git Repository

--- AutomationProject_Parthi/src/Handling_New_Git.java
+++ AutomationProject_Parthi/src/Handling_New_Git.java
@@ -3,7 +3,10 @@ public class Handling_New_Git {
 
 	public static void main(String[] args) {
 		System.out.println("New Git file with Updation");
+		System.out.println("Hi Ram");
+			
+		}
 
 	}
 
-}
+
